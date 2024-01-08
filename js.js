71310edc3f569674ad003740860aac59eb08eaa0
HiimAndrew <125762486+HiimAndrew123@users.noreply.github.com>
2024-01-08 17:38:08 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,12 @@
+var x = Math.floor(Math.random()*2) // x is to randomize the text
+var textarray = [ // modify this if you want
+  "Made with Maxi Toys's help!!",
+  "see that scrolling bar at the bottom? thats just a bug and idk if i can fix it :/",
+]
+document.getElementById("splash").innerHTML = textarray[x]; // "innerHTML" is to modify the text and "getElementById("splash")" is to get the element with id "splash"document.getElementById("splash").innerHTML = textarray[x]; // "innerHTML" is to modify the text and "getElementById("splash")" is to get the element with id "splash"
+function channelredirect() {
+  window.location.href = "https://www.youtube.com/channel/UC1NHOa6q5fKAVa_zERKPfog"; // your yt or other
+}
+function discordredirect() {
+  window.location.href = "https://discord.gg/THnUrg3F64"; // your social media or other
+}
